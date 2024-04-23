93c80a0f7a994e3276d66e658d9a3f6c27e7b665
Ryan <r803425@gmail.com>
2024-04-24 01:38:12 +0800

    商品功能 - 實作刪除商品

--- src/main/java/com/ryan/sprintbootmall/controller/ProductController.java
+++ src/main/java/com/ryan/sprintbootmall/controller/ProductController.java
@@ -52,4 +52,11 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
     }
 
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable Integer productId) {
+        productService.deleteProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 }
