7b836970324230c3d3de76984de1628ed8918dad
Ryan <r803425@gmail.com>
2024-04-23 23:56:09 +0800

    商品功能 - 實作查詢商品

---
+++ src/main/java/com/ryan/sprintbootmall/dao/ProductDao.java
@@ -0,0 +1,8 @@
+package com.ryan.sprintbootmall.dao;
+
+import com.ryan.sprintbootmall.model.Product;
+
+public interface ProductDao {
+
+    Product getProductById(Integer productId);
+}
---
+++ src/main/java/com/ryan/sprintbootmall/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.ryan.sprintbootmall.service;
+
+import com.ryan.sprintbootmall.model.Product;
+
+public interface ProductService {
+
+    Product getProductById(Integer productId);
+}
