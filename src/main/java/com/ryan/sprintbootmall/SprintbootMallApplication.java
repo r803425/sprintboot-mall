0c79e92946beb7649cdaa5f5c05db71abecc2a22
Ryan <r803425@gmail.com>
2024-04-23 23:12:46 +0800

    初始化 Spring Boot Project

---
+++ src/main/java/com/ryan/sprintbootmall/SprintbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.sprintbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootMallApplication.class, args);
+	}
+
+}
