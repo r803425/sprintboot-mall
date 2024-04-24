2ec57eebef39c203feb0a0043eaa63f5edc8128b
Ryan <r803425@gmail.com>
2024-04-24 13:40:14 +0800

    商品功能 - 實作查詢商品列表 (排序)

--- src/main/java/com/ryan/sprintbootmall/dto/ProductQueryParams.java
+++ src/main/java/com/ryan/sprintbootmall/dto/ProductQueryParams.java
@@ -1,12 +1,13 @@
 package com.ryan.sprintbootmall.dto;
 
 import com.ryan.sprintbootmall.constant.ProductCategory;
-import org.springframework.web.bind.annotation.RequestParam;
 
 public class ProductQueryParams {
 
     private ProductCategory category;
     private String search;
+    private String orderBy;
+    private String sort;
 
     public ProductCategory getCategory() {
         return category;
@@ -22,5 +23,21 @@ public class ProductQueryParams {
 
     public void setSearch(String search) {
         this.search = search;
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
     }
 }
