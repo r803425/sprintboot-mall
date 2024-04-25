050b9f9b39aa1258d492f671767a69886d87b9e7
Ryan <r803425@gmail.com>
2024-04-26 01:23:08 +0800

    帳號功能 - 檢查註冊的 email、隱藏返回的密碼

--- src/main/java/com/ryan/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/ryan/springbootmall/service/impl/UserServiceImpl.java
@@ -4,11 +4,17 @@ import com.ryan.springbootmall.dao.UserDao;
 import com.ryan.springbootmall.dto.UserRegisterRequest;
 import com.ryan.springbootmall.model.User;
 import com.ryan.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
@@ -20,6 +26,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        // 檢查註冊的email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if (user != null) {
+            log.warn("該 email {} 已經被註冊", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        // 創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 }
