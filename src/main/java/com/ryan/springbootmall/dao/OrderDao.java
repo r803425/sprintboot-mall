15bb994b492ca91f9b6dceed748aed2ef67f8660
Ryan <r803425@gmail.com>
2024-04-27 04:39:17 +0800

    訂單功能 - 查詢訂單列表

--- src/main/java/com/ryan/springbootmall/dao/OrderDao.java
+++ src/main/java/com/ryan/springbootmall/dao/OrderDao.java
@@ -1,11 +1,16 @@
 package com.ryan.springbootmall.dao;
 
+import com.ryan.springbootmall.dto.OrderQueryParams;
 import com.ryan.springbootmall.model.Order;
 import com.ryan.springbootmall.model.OrderItem;
 
 import java.util.List;
 
 public interface OrderDao {
+
+    Integer countOrder(OrderQueryParams orderQueryParams);
+
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
 
     Order getOrderById(Integer orderId);
 
--- src/main/java/com/ryan/springbootmall/service/OrderService.java
+++ src/main/java/com/ryan/springbootmall/service/OrderService.java
@@ -1,9 +1,16 @@
 package com.ryan.springbootmall.service;
 
 import com.ryan.springbootmall.dto.CreateOrderRequest;
+import com.ryan.springbootmall.dto.OrderQueryParams;
 import com.ryan.springbootmall.model.Order;
 
+import java.util.List;
+
 public interface OrderService {
+
+    Integer countOrder(OrderQueryParams orderQueryParams);
+
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
 
     Order getOrderById(Integer orderId);
 
